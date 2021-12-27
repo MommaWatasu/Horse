a51971e478a0d99358928f9de99cf8c75eaad251
QGMW22 <ascendwatson@gmail.com>
2021-12-28 08:38:10 +0900

    [update]update kernel
    build kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,11 +3,26 @@
 #![feature(asm)]
 #![feature(abi_efiapi)]
 
+use core::panic::PanicInfo;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct FrameBufferInfo {
+    pub fb: *mut u8,
+    pub size: usize,
+}
+
 #[no_mangle]
-extern "efiapi" fn kernel_main(fb_config: FrameBufferConfig) -> ! {
+extern "sysv64" fn kernel_main(fb_config: FrameBufferInfo) -> ! {
     loop {
         unsafe {
             asm!("hlt")
         }
     }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    //error!("{}", info);
+    loop {}
 }
