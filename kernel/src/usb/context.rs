86d5e6659f817210602bcc189a3a7cfe5dec3b55
QGMW22 <ascendwatson@gmail.com>
2022-01-02 00:01:03 +0900

    [add function or file]add derive for some Context
    use comparison operators

--- kernel/src/usb/context.rs
+++ kernel/src/usb/context.rs
@@ -1,14 +1,17 @@
 #[repr(C, align(32))]
+#[derive(PartialEq)]
 pub struct SlotContext {
     data: [u32; 8],
 }
 
 #[repr(C, align(32))]
+#[derive(PartialEq)]
 pub struct EndpointContext {
     data: [u32; 8],
 }
 
 #[repr(C, align(64))]
+#[derive(PartialEq)]
 pub struct DeviceContext {
     slot_context: SlotContext,
     endpoint_contexts: [EndpointContext; 31],
